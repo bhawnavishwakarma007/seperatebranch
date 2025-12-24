815105a6a689a2f3ef65fa953bf29014041e25d1
Bhawna Vishwakarma <159766023+bhawnavishwakarma007@users.noreply.github.com>
2025-12-24 10:29:29 +0530

    Update log message for server startup

--- app.js
+++ app.js
@@ -18,5 +18,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
